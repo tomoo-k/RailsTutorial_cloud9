4a2a75abbc03901dd8c40381918b36606970dbd0
Tomoo Kameyama <tomoo.kameyama@gmail.com>
2020-03-26 03:18:13 +0900

    8.4f

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # 渡された文字列の最小コストのハッシュ値を返すfixture向け
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
